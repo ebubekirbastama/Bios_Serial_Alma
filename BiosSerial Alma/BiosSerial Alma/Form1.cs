c802fd03c94fde3f88d09d79453204ee408a5572
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2023-07-10 11:37:52 +0300

    Add files via upload

---
+++ BiosSerial Alma/BiosSerial Alma/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Management;
+using System.Windows.Forms;
+
+namespace BiosSerial_Alma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string a, b, c;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string relDt = "";
+            try
+            {
+                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT SerialNumber, SMBIOSBIOSVersion, ReleaseDate FROM Win32_BIOS");
+                ManagementObjectCollection collection = mSearcher.Get();
+                foreach (ManagementObject obj in collection)
+                {
+                    a= (string)obj["SerialNumber"];
+                    b = (string)obj["SMBIOSBIOSVersion"];
+                    relDt = (string)obj["ReleaseDate"];
+                    DateTime dt = ManagementDateTimeConverter.ToDateTime(relDt);
+                    c = dt.ToString("dd-MMM-yyyy");//date format
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
